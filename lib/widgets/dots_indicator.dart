fd0a1ffa7c2b4a0373f0eb5db6a1bafdcad96b00
Shimaa Elnaggar <shimaaibrahim072@gmail.com>
2024-07-31 17:41:19 +0300

    create dots indicator

---
+++ lib/widgets/dots_indicator.dart
@@ -0,0 +1,26 @@
+
+import 'package:flutter/material.dart';
+
+class DotsIndicator extends StatelessWidget {
+  final bool  isActive;
+  const DotsIndicator({
+    required this.isActive,
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(2.0),
+      child: AnimatedContainer(
+        duration: const Duration(milliseconds: 300),
+        height: 5,
+        width: 20,
+        decoration: BoxDecoration(
+          borderRadius:BorderRadius.circular(20),
+          color: isActive ? Theme.of(context).primaryColor : Colors.black,
+        ),
+      ),
+    );
+  }
+}
