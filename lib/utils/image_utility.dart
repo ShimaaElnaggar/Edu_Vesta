1924072f8d3888d84760b3867da9f33f5de018ec
Shimaa Elnaggar <shimaaibrahim072@gmail.com>
2024-07-31 16:33:51 +0300

    create image utility

---
+++ lib/utils/image_utility.dart
@@ -0,0 +1,9 @@
+class ImageUtility{
+  static const String _path = 'assets/images/' ;
+  static const String logo = '${_path}logo.png';
+  static const String badges = '${_path}badges.png';
+  static const String courseCatalog = '${_path}course_catalog.png';
+  static const String frame = '${_path}frame.png';
+  static const String offlineAccess = '${_path}offline_access.png';
+  static const String progressTracking = '${_path}progress_tracking.png';
+}
